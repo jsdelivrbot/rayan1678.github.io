4da83cb7802ebbcb1d971e054a39f88f3b8083e3
Rayan <drgnninja662@gmail.com>
2016-07-19 20:00:35 +0000

    That button

--- That_was_easy/main.js
+++ That_was_easy/main.js
@@ -3,3 +3,10 @@ function sayThatWasEasy() {
     thatWasEasy.play();
 }
 $("#easy").on("click", sayThatWasEasy);
+$(document).keypress(delegateKeypress)
+
+function delegateKeypress(event){
+    if (event.charcode ==32){
+        $(easy).trigger("click")
+    }
+}
