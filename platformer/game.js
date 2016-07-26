82110ef8e30cfcf78a857221bf9a2ae33ead923a
Rayan <drgnninja662@gmail.com>
2016-07-26 18:06:42 +0000

    fixed overlap now need to fix click

--- platformer/game.js
+++ platformer/game.js
@@ -10,20 +10,24 @@ var isGameOver;
 var score;
 
 function setup() {
-    isGameOVer = false;
+    
+    isGameOver = false;
     score = 0;
     
     createCanvas(600,400);
-    background(400,300, 250);
+    background(150,200, 250);
     groundSprites = new Group();
     
-    numGroundSprites = width/GROUND_SPRITE_WIDTH;
+    numGroundSprites = width/GROUND_SPRITE_WIDTH+1;
+    
     for (var n = 0; n < numGroundSprites; n++) {
         var groundSprite = createSprite(n*50, height-25, GROUND_SPRITE_WIDTH, GROUND_SPRITE_HEIGHT);
         groundSprites.add(groundSprite);
-        player = createSprite(100, height-75, 50, 50);
-        obstacleSprites = new Group();
     }
+        player = createSprite(100, height-75, 50, 50);
+        
+        obstacleSprites = new Group();
+
     
 }
 
@@ -32,8 +36,8 @@ function draw(){
         background(0);
         fill(255);
         textAlign(CENTER);
-        text("Your score was: " + score, camera.position.y - 20);
-        text("Game OVer! Clcik to Restart", camera.position.x, camera.position.y);
+        text("Your score was: " + score, camera.position.x, camera.position.y - 20);
+        text("Game Over! Click to restart", camera.position.x, camera.position.y);
         
     } else {
     background(150, 200, 250);
@@ -43,11 +47,20 @@ function draw(){
         player.velocity.y = 0;
         player.position.y = (height-50) - (player.height/2);
     }
+
     if (keyDown(UP_ARROW)) {
         player.velocity.y = JUMP;
     }
-    player.position.x =player.position.x + 5;
-    if (random () > 0.95) {
+    player.position.x = player.position.x + 5;
+    camera.position.x = player.position.x + (width/4);
+    
+    var firstGroundSprite = groundSprites[0];
+    if (firstGroundSprite.position.x <= camera.position.x - (width/2 + firstGroundSprite.width/2)) {
+        groundSprites.remove(firstGroundSprite);
+        firstGroundSprite.position.x = firstGroundSprite.position.x + numGroundSprites*firstGroundSprite.width;
+        groundSprites.add(firstGroundSprite);
+    }
+    if (random() > 0.95) {
         var obstacle = createSprite(camera.position.x + width, random(0, (height-50)-15), 30, 30);
         obstacleSprites.add(obstacle);
      }
@@ -55,36 +68,34 @@ function draw(){
      if (obstacleSprites.length > 0 && firstObstacle.position.x <= camera.position.x - (width/2 + firstObstacle.width/2)) {
          removeSprite(firstObstacle);
      }
-     obstacleSprites.overlap(player, endGame);
+     
+    obstacleSprites.overlap(player, endGame);
    drawSprites();
    score = score + 1;
    textAlign(CENTER);
    text(score, camera.position.x, 10);
     camera.position.x = player.position.x + (width/4);
-    var firstGroundSprite = groundSprites[0];
-    if (firstGroundSprite.position.x <= camera.position.x - (width/2 + firstGroundSprite.width/2)) {
-        groundSprites.remove(firstGroundSprite);
-        firstGroundSprite.position.x = firstGroundSprite.position.x + numGroundSprites*firstGroundSprite.width;
-        groundSprites.add(firstGroundSprite);
+    
     }
 }
-}
+
 function endGame() {
-    isGameover = true;
+    isGameOver = true;
 }
- function mouseClicked() {
-     if (isGameOver) {
-     for (var n = 0; n < numGroundSprites; n++) {
-         var groundSprite = groundSprites[n];
-         groundSprite.position.x = n*50;
-     }
-         player.position.x = 100;
-         player.position.y = height-75;
+function mouseClicked() {
+    console.log
+    if (isGameOver) {
+        for (var n = 0; n < numGroundSprites; n++) {
+            var groundSprite = groundSprites[n];
+            groundSprite.position.x = n*50;
+        }
+        player.position.x = 100;
+        player.position.y = height-75;
         
-         obstacleSprites.removesSprites();
+        obstacleSprites.removesSprites();
+
         score = 0;
-        
-         isGameOVer = false; 
+        isGameOver = false; 
      }
  }
     
