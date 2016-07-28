32f2a9c4bece5d7244815cfcc45f7e7522cd37c0
Rayan <drgnninja662@gmail.com>
2016-07-28 22:29:19 +0000

    reresubmit

--- Heaven/main.js
+++ Heaven/main.js
@@ -1,7 +1,7 @@
 
 $('#Us').click(function() {
   window.location='https://www.youtube.com/watch?v=IhnUgAaea4M';
-});
+}); 
 
 $('#cold').click(function() {
   window.location='https://www.youtube.com/watch?v=Ll1seZzlF0o';
