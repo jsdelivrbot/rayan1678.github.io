0536a4898552b0087db1ebcb2ae62edf18021a75
Rayan <drgnninja662@gmail.com>
2016-07-28 22:11:41 +0000

    welcome to heaven

---
+++ Button 2.0/main.js
@@ -0,0 +1,5 @@
+function sayXd() {
+    var xd = new Audio("feels_bad_man_k.mp3");
+    xd.play();
+}
+$("#xd").on("click", sayXd);
---
+++ Heaven/main.js
@@ -0,0 +1,8 @@
+
+$('#Us').click(function() {
+  window.location='https://www.youtube.com/watch?v=IhnUgAaea4M';
+});
+
+$('#cold').click(function() {
+  window.location='https://www.youtube.com/watch?v=Ll1seZzlF0o';
+});
